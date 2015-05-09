8060aaeaa71eb052eb3507e1756416e729e77e53
Vlad Velici <vlad.velici@gmail.com>
2015-05-09 19:46:34 +0100

    initial form version

---
+++ js/animations.js
@@ -0,0 +1,67 @@
+(function (window) {
+    var requestAnimFrame = (function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||function(callback){window.setTimeout(callback,1000/60);};})();
+
+    var easeInOutQuad = function (t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    };
+
+    var animatedScrollTo = function (element, to, duration, callback) {
+        var start = element.scrollTop,
+        change = to - start,
+        animationStart = +new Date();
+        var animating = true;
+        var lastpos = null;
+
+        var animateScroll = function() {
+            if (!animating) {
+                return;
+            }
+            requestAnimFrame(animateScroll);
+            var now = +new Date();
+            var val = Math.floor(easeInOutQuad(now - animationStart, start, change, duration));
+            if (lastpos) {
+                if (lastpos === element.scrollTop) {
+                    lastpos = val;
+                    element.scrollTop = val;
+                } else {
+                    animating = false;
+                }
+            } else {
+                lastpos = val;
+                element.scrollTop = val;
+            }
+            if (now > animationStart + duration) {
+                element.scrollTop = to;
+                animating = false;
+                if (callback) { callback(); }
+            }
+        };
+        requestAnimFrame(animateScroll);
+    };
+
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = animatedScrollTo;
+    } else {
+        window.animatedScrollTo = animatedScrollTo;
+    }
+})(window);
+
+window.addEventListener("load", function() {
+  var labels = document.getElementsByClassName("ss-q-short");
+
+  for (var i=0; i<labels.length; i++) {
+    labels[i].addEventListener("focus", function(e) {
+      e.target.parentNode.classList.add("focused");
+      var pos = window.scrollY + e.target.getBoundingClientRect().top - window.innerHeight/2;
+      window.animatedScrollTo(document.body, pos, 300, function() {});
+    });
+    labels[i].addEventListener("blur", function(e) {
+      e.target.parentNode.classList.remove("focused");
+    });
+  }
+
+
+});
